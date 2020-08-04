ee8e549f839f90ea3d21b01e0e23d33e18308ea2
Tweek123 <swapep@mail.com>
2020-08-04 23:27:00 +0300

    deploy

--- magazin_dns/settings.py
+++ magazin_dns/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static/polls/images')
+    os.path.join(BASE_DIR, 'media')
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
