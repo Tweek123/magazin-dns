8b31f97e91302d7eb12d68436a9baee29242d8db
Tweek123 <swapep@mail.com>
2020-08-04 13:02:09 +0300

    deploy

--- magazin_dns/settings.py
+++ magazin_dns/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
