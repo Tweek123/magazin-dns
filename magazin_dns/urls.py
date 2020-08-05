0583b184389d3e7d0d6e5e9c7818afa31cd48dd5
Tweek123 <swapep@mail.com>
2020-08-05 14:22:21 +0300

    deploy

--- magazin_dns/urls.py
+++ magazin_dns/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     url(r'^paypal/', include('paypal.standard.ipn.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
